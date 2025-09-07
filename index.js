9a099a9a39f4b22d56e0877bb2120d62c44fb948
AhmedSaid25 <ahmedmegho460@gmail.com>
2025-09-07 21:14:54 +0200

    Support CORS

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const limiterRoutes = require("./routes/limiter");
 const errorHandler = require("./middleware/errorHandler");
+const cors = require("cors");
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 // Routes
 app.use("/", limiterRoutes);
